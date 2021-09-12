9301458bde464f3ad0f5a44d0ecf085f6f9a8f5e
Denis-leparteleg <denis.leparteleg@student.moringaschool.com>
2021-09-12 20:11:20 +0300

    add jQuery toggle functionality to whatwedo section

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// whatwedo toggle functionality
+$(document).ready(function(){
+    $("#design").click(function(){
+      $("#designimg").toggle();
+      $("#designdesc").toggle();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#dev").click(function(){
+      $("#devimg").toggle();
+      $("#development").toggle();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#management").click(function(){
+      $("#productimg").toggle();
+      $("#product").toggle();
+    });
+  });
